b0cf39576fa1c8fc3f95826bd26b5f99d436166a
jstutul <jaforsadiktutul9228@gmail.com>
2022-09-13 00:49:29 +0600

    dirst commit

--- users/urls.py
+++ users/urls.py
@@ -18,9 +18,6 @@ urlpatterns=[
     path('reset-password/complete/',
          PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
          name='password_reset_complete'),
-
-
-    path('password-change/',views.PasswordChange,name="passwordchange"),
     path('logout/',views.Logout,name="logout"),
     path('contract-us',views.ContactUs,name='contact'),
     path('contest-home/',views.ContestHome,name="contesthome"),
@@ -32,19 +29,7 @@ urlpatterns=[
     path('contest/love',views.ContestLove,name="contestlove"),
     path('remove-photo/',views.RemovePhoto,name="removephoto"),
     path('decleare-winner/<int:contest>/<int:photom>',views.Winner,name="winner"),
-    path('dashboard/',views.Dashboard,name="dashboard"),
-    path('profile/',views.UserProfile,name="profile"),
-    path('photo/update',views.PHotoUpdate,name="photoupdate"),
-    path('add-contest/',views.AddCont,name="addcontest"),
-    path('view-contest/',views.ManageContest,name="managecontest"),
-    path('delete-contest/',views.DeleteContest,name="deletecontest"),
-    path('update-contest/<int:id>',views.UpdateContest,name="updatecontest"),
-    path('add-balance/',views.AddBalance,name="addbalance"),
-    path('completed/',views.paymentComplated,name="completed"),
-    path('request-payment/',views.RequestPayment,name="requestpayment"),
     path('gallary-view/',views.Galary,name="galary"),
     path('about-me/',views.AboutmeDetails,name='aboutme'),
 
-    # Feedback url
-    path('feedback/',views.FeedbackView,name='feedback'),
     ]
